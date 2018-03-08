38a1518d3883d93ee7b08c6e45aadd4886961c41
Your Name <you@example.com>
2018-03-08 16:55:15 +0200

    five+

--- app/js/common.js
+++ app/js/common.js
@@ -109,3 +109,139 @@ $('.table_coin').click(function() {
   }]
   });
   });
+
+
+
+  // chart comparison
+
+
+
+      var seriesOptions = [],
+        seriesCounter = 0,
+        names = ['BTC','MSFT', 'AAPL', 'GOOG'];
+
+      /**
+       * Create the chart when all data is loaded
+       * @returns {undefined}
+       */
+      function createChart() {
+
+        Highcharts.stockChart('compare_chart', {
+
+          chart:{
+             // spacingTop: 10,
+          },
+
+          title: {
+            text: 'Cryptocurrency Comparison',
+            align: 'left',
+             // y: -10,
+            // margin: 40,
+            style: {
+              "color": "#222222",
+              "fontSize": "35px",
+              "fontWeight": "300"
+            }
+          },
+          style: {
+            "fontFamily": "OpenSans",
+          },
+          legend: {
+            itemStyle: {
+              "fontSize": "14px",
+              "fontWeight": "normal"
+            },
+            itemMargin: 10,
+            padding:40,
+            enabled: true,
+            title: {
+              text: 'Choose Cryptocurency      <br> for Composition',
+              style: {
+                "color": "#222222",
+                "font-size": "14px",
+                "line-height": "15px",
+                "fontWeight": "bold",
+                "margin": "50px"
+              }
+            },
+            align: 'right',
+            verticalAlign: 'top',
+            layout: 'vertical',
+            x: 0,
+            y: 100,
+            borderColor: '#dbdbdb',
+            borderWidth: 1,
+          },
+          rangeSelector: {
+            selected: 4
+          },
+          colors: ['#79b54d', '#414b56', '#ff4504'],
+          yAxis: {
+
+            labels: {
+              formatter: function() {
+                return (this.value > 0 ? ' + ' : '') + this.value + '%';
+              }
+            },
+            plotLines: [{
+              value: 0,
+              width: 2,
+              color: 'silver'
+            }]
+          },
+          navigator: {
+            enabled: false
+          },
+
+          plotOptions: {
+            series: {
+
+              compare: 'percent',
+              showInNavigator: true,
+              marker: {
+                fillColor: '#FFFFFF',
+                lineWidth: 2,
+                lineColor: null,
+                symbol: 'circle',
+                states: {
+                    hover: {
+                        radiusPlus: 3,
+                        lineWidthPlus: 2,
+                        // lineWidth: "5px",
+                        // lineColor: "#fff"
+                    }
+                },
+              }
+            }
+
+          },
+
+
+          tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+            valueDecimals: 2,
+            split: true
+          },
+
+          series: seriesOptions,
+        });
+      }
+
+      $.each(names, function(i, name) {
+
+        $.getJSON('js/' + name.toLowerCase() + '-c.json', function(data) {
+
+          seriesOptions[i] = {
+            type: 'spline',
+            name: name,
+            data: data,
+
+          };
+
+          seriesCounter += 1;
+
+          if (seriesCounter === names.length) {
+            createChart();
+          }
+        });
+      });
